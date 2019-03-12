8048f264d8722a01b10a72dc9bafa6d339d2b5e0
Marcus Teixeira <vbt.marcus@gmail.com>
2019-03-12 03:58:06 -0300

    foi a interações com o JS para que se o usuario não coloque algo o documento se modifique, apontando os erros no preencimento

---
+++ js/validacao.js
@@ -0,0 +1,22 @@
+function validaFormulario(fm) {
+	if(fm.usuario.value=="" || fm.senha.value==""){
+		document.getElementById("mensagem").innerHTML = "Atenção: Alguns dos campos não foi preenchido";
+		
+		if(fm.senha.value==""){
+			document.getElementById("senha").style.border="solid red 2px";
+		}else{
+			document.getElementById("senha").style.border="solid black 2px";
+		}
+		
+		if(fm.usuario.value==""){
+			document.getElementById("usuario").style.border="solid red 2px";
+		}else{
+			document.getElementById("usuario").style.border="solid black 2px";
+		}
+		
+		document.getElementById("centraliza").style.height="200px";
+		document.getElementById("centraliza").style.marginTop="-150px";
+		
+		return false;
+	}
+}
